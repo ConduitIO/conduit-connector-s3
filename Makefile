dd20a457c2ceada0546088dda0c86ec5f0b703af
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-09-21 14:22:11 +0200

    Add Goreleaser (#122)
    * add goreleaser
    
    * fixes
    
    * makefile version

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 .PHONY: build test
 
+VERSION=$(shell git describe --tags --dirty --always)
+
 build:
-	go build -o conduit-connector-s3 cmd/s3/main.go
+	go build -ldflags "-X 'github.com/conduitio/conduit-connector-s3.version=${VERSION}'" -o conduit-connector-s3 cmd/connector/main.go
 
 test:
 	go test $(GOTEST_FLAGS) -race ./...
--- spec.go
+++ spec.go
@@ -18,14 +18,17 @@ import (
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
-type Spec struct{}
+// version is set during the build process (i.e. the Makefile).
+// It follows Go's convention for module version, where the version
+// starts with the letter v, followed by a semantic version.
+var version = "v0.0.0-dev"
 
 // Specification returns the Plugin's Specification.
 func Specification() sdk.Specification {
 	return sdk.Specification{
 		Name:    "s3",
 		Summary: "An S3 source and destination plugin for Conduit, written in Go.",
-		Version: "v0.1.0",
+		Version: version,
 		Author:  "Meroxa, Inc.",
 	}
 }
